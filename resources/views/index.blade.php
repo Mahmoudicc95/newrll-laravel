34147e174063e43b267a6579d453eec863c70664
Mahmoudicc95 <mahmoud-icc@hotmail.com>
2022-01-25 16:49:14 +0800

    localization config

---
+++ resources/views/index.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Localization</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Localization</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav ml-auto flex-nowrap">
+        <li class="nav-item text-white p-2"><a class="btn text-white"href="#">Home</a></li>
+        <li class="nav-item text-white p-2"><a class="btn text-white"href="#">About us</a></li>
+        <li class="nav-item text-white p-2"><a class="btn text-white"href="#">Contact </a></li>
+      </ul>
+      <ul class="navbar-nav ml-auto flex-nowrap">
+          <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle text-danger" href="#" id="navbardropdown" role="button" data-toggle="dropdown" aria-haspopup="">
+                language</a>
+                <div class="dropdown-menu"aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">English<i></i></a>
+                </div>
+          </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</body>
+</html>
