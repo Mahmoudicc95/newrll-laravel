2b0b692be27b99938e08ce3aeb6fe50935ee09a3
mahmoudicc95 <mahmoud-icc@hotmail.com>
2022-02-09 15:24:32 +0800

    insertting more data to database table

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,17 +10,17 @@ class UserController extends Controller
 {
     function index()
     {
-         return User::find(2);
-        // return Phone::find(3);
+        //  return User::find(1);
+        return Phone::find(2);
     }
     
     public function insertRecord()
     {
         $phone = new Phone();
-        $phone->phone = "01160605280";
+        $phone->phone = "0116060";
         $user = new user();
-        $user->name = "Mahmoud";
-        $user->email = "Mahmoud.@gmail.com";
+        $user->name = "Mahmdd";
+        $user->email = "Mahmdd.@gmail.com";
         $user->password = encrypt('431234');
         $user->save();
         $user->phone()->save($phone);
